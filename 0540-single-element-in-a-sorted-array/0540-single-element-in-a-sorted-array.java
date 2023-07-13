7547c41b2346cc450ec538a94e0405c90877c7b5
Aryan Gupta <77088580+a-ryan2@users.noreply.github.com>
2023-07-14 00:49:25 +0530

    Time: 0 ms (100.00%), Space: 50 MB (70.80%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -6,9 +6,24 @@ class Solution {
         if(nums[nums.length-1]!=nums[nums.length-2]){
             return nums[nums.length-1];
         }
-        for(int i=1;i<nums.length-1;i++){
-            if(nums[i]!=nums[i+1]&&nums[i]!=nums[i-1]){
-                return nums[i];
+        // for(int i=1;i<nums.length-1;i++){
+        //     if(nums[i]!=nums[i+1]&&nums[i]!=nums[i-1]){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+        int low=1;
+        int high=nums.length-2;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }
+            else if((mid%2==0 &&nums[mid]==nums[mid+1])||(mid%2==1&&nums[mid]==nums[mid-1])){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
         return -1;
