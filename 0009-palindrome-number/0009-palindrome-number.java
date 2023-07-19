cfb35269c733f7d5bc62dd3435bfbfc82a9f3a77
Aryan Gupta <77088580+a-ryan2@users.noreply.github.com>
2023-07-19 18:49:47 +0530

    Time: 10 ms (54.02%), Space: 42.9 MB (63.46%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -4,17 +4,31 @@ class Solution {
         if(s.length()==1){
             return true;
         }
-        if(s.length()==2){
-            if(s.charAt(0)!=s.charAt(1)){
-                return false;
-            }
-            return true;
-        }
+        // if(s.length()==2){
+        //     if(s.charAt(0)!=s.charAt(1)){
+        //         return false;
+        //     }
+        //     return true;
+        // }
         for(int i=0;i<(s.length())/2;i++){
             if(s.charAt(i)!=s.charAt(s.length()-1-i)){
                 return false;
             }
         }
         return true;
+        
+        
+        // while(x>=0){
+        //     int p=x%10;
+        //     x=x/10;
+        //     arr.add(p);
+        // }
+        // int n=arr.size();
+        // for(int i=0;i<n/2;i++){
+        //     if(arr.get(i)!=arr.get(n-1-i)){
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 }
