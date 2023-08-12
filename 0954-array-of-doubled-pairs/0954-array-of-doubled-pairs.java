573905453a45f4ed21ccf4298d87301ed78d1c64
Aryan Gupta <77088580+a-ryan2@users.noreply.github.com>
2023-08-12 23:43:23 +0530

    Time: 87 ms (28.65%), Space: 51.1 MB (65.10%) - LeetHub

---
+++ 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean canReorderDoubled(int[] A) {
+        // for(int i=0;i<arr.length/2;i++){
+        //     if(arr[2*i+1]!=2*arr[2*i]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+         Map<Integer, Integer> count = new HashMap();
+        for (int x: A)
+        {
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+        Integer[] B = new Integer[A.length];
+        for (int i = 0; i < A.length; ++i)
+        {
+            B[i] = A[i] ;
+        }
+        Arrays.sort(B, Comparator.comparingInt(Math::abs));
+        for (int x: B) 
+        {
+            if(count.get(x) == 0)
+            {
+                continue ;
+            }
+            if(count.getOrDefault(2*x, 0) <= 0) 
+            {
+                return false;
+            }
+            count.put(x, count.get(x) - 1);
+            count.put(2*x, count.get(2*x) - 1);
+        }
+        return true;
+    }
+}
