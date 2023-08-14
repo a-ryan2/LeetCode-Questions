55bbd2e1626351cbc2c00d58e1efb02986613bea
Aryan Gupta <77088580+a-ryan2@users.noreply.github.com>
2023-08-15 02:29:24 +0530

    Added solution - LeetHub

---
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+   
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr (int A[], int N, int k) {
+        HashMap<Integer,Integer> s=new HashMap<>();
+        int presum=0;
+        int res=0;
+        for(int i=0;i<A.length;i++){
+            presum+=A[i];
+            if(presum==k){
+                res=i+1;
+            }
+            if(s.containsKey(presum)==false){
+                s.put(presum,i);
+            }
+            if(s.containsKey(presum-k)){
+                res=Math.max(res,i-s.get(presum-k));
+            }
+            
+        }
+        return res;
+    }
+    
+    
+}
+
+
